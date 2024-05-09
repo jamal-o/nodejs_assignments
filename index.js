11423670182cea31b1e0bd348b1e42a53b5e0b32
jamal-o <jamaldeenolasupo@gmail.com>
2024-05-09 19:56:41 +0100

    Add Cors headers

--- index.js
+++ index.js
@@ -35,6 +35,10 @@ function handleGetRequest(req, res) {
 }
 const requestHandler = (req, res) => {
 
+    res.setHeader('Access-Control-Allow-Origin', '127.0.0.1');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS'); 
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization'); 
+
     if (req.method === "GET"){
         handleGetRequest(req, res);
     }else{
